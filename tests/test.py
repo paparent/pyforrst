c3b801d9de8101af0f2d01f59af3517d89fa8590
Luke Lee <durdenmisc@gmail.com>
2010-09-11 22:33:23 -0500

    Added tests for user_info_by_id - Refactored test asserts into helper method

--- tests/test.py
+++ tests/test.py
@@ -18,11 +18,11 @@ class TestUserInfo(unittest.TestCase):
     Test the user_info API function
     """
 
-    def test_user_info_success(self):
+    def _verify_test_user(self, user):
         """
-        Verify that user_info() returns correct data with valid username
+        Helper method for verifying user data against test user as defined by
+        TEST_USERNAME and TEST_USERID
         """
-        user = pyforrst.user_info(TEST_USERNAME)
         self.assertTrue('username' in user, 'Username key not in return value')
         self.assertTrue('id' in user, 'ID key not in return value')
         self.assertEqual(user['username'], TEST_USERNAME,
@@ -34,12 +34,31 @@ class TestUserInfo(unittest.TestCase):
                          "User ID requested: %d returned: %d" % \
                          (TEST_USERID, int(user['id'])))
 
+    def test_user_info_success(self):
+        """
+        Verify that user_info() returns correct data with valid username
+        """
+        self._verify_test_user(pyforrst.user_info(TEST_USERNAME))
+
     def test_user_info_invalid_username(self):
         """
         Verify that user_info() returns an error when given invalid user
         """
         self.assertRaises(pyforrst.ForrstError, pyforrst.user_info,
                             'INVALIDUSER123')
+
+    def test_user_info_id_success(self):
+        """
+        Verify that user_info_by_id() returns correct data with valid user id
+        """
+        self._verify_test_user(pyforrst.user_info_by_id(TEST_USERID))
+
+    def test_user_info_id_invalid_id(self):
+        """
+        Verify that user_info_by_id() returns an error when given invalid id
+        """
+        self.assertRaises(pyforrst.ForrstError, pyforrst.user_info_by_id, -1)
+
 
 if __name__ == '__main__':
     unittest.main()
