19830c7f553be896a8dc7fc93a0d6811138f83cb
PA Parent <paparent@paparent.me>
2010-09-19 16:36:23 -0400

    Move from distutils to setuptools
    Tests can be executed by: python setup.py test

--- setup.py
+++ setup.py
@@ -1,27 +1,33 @@
 import os
-from distutils.core import setup
+from setuptools import setup
 
 from pyforrst import __version__
 
-setup(name='pyforrst',
-      version=__version__,
-      author='PA Parent',
-      author_email='paparent@paparent.me',
-      description='Python interface to Forrst API',
-      long_description=file(
-          os.path.join(
-              os.path.dirname(__file__),
-              'README'
-            )
-      ).read(),
-      license="MIT",
-      url="http://github.com/paparent/pyforrst",
-      py_modules=['pyforrst'],
-      classifiers=[
-          "Development Status :: 3 - Alpha",
-          "Programming Language :: Python",
-          "Intended Audience :: Developers",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-          "License :: OSI Approved :: MIT License",
-      ],
-     )
+setup(
+    name='pyforrst',
+    version=__version__,
+    author='PA Parent',
+    author_email='paparent@paparent.me',
+    description='Python interface to Forrst API',
+    long_description=file(
+        os.path.join(
+            os.path.dirname(__file__),
+            'README'
+          )
+    ).read(),
+    license="MIT",
+    url="http://github.com/paparent/pyforrst",
+    py_modules=['pyforrst'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    exclude_package_data={'': ['*.pyc']},
+    install_requires=['setuptools'],
+    test_suite = 'tests',
+)
